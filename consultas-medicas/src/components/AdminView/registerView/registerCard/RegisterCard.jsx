6ba3feb48614fbd826269c3d409d8760cc376f7c
Marco Ramirez <mvrcentes@hotmail.com>
2023-03-29 18:20:51 -0600

    change style for registerCard

--- consultas-medicas/src/components/AdminView/registerView/registerCard/RegisterCard.jsx
+++ consultas-medicas/src/components/AdminView/registerView/registerCard/RegisterCard.jsx
@@ -1,10 +1,35 @@
-const RegisterCard = ({options}) => {
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+
+//style
+import "./registerCard.css";
+
+const RegisterCard = ({ nameLastName, entityName, entityAddress }) => {
     return (
         <div className="registerCard">
             <div className="fila">
-                {options.map((option) => (
-                    <div className="text">{option}</div>
-                ))}
+                <div className="nameLastName">{nameLastName}</div>
+                {/* <div className="entityName">{entityName}</div> */}
+                <Dropdown className="entityNameDropdown" as={ButtonGroup}>
+                    <Button className="entityNameButton">{entityName}</Button>
+
+                    <Dropdown.Toggle
+                        split
+                        id="dropdown-split-basic"
+                    />
+
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">
+                            Another action
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">
+                            Something else
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <div className="entityAddress">{entityAddress}</div>
             </div>
         </div>
     );
