fa1e92769658605a392ae4becc7bd6961acc17e2
Marco Ramirez <mvrcentes@hotmail.com>
2023-03-30 14:07:46 -0600

    add AdminViewDoctors view

---
+++ consultas-medicas/src/components/AdminView/adminViewDoctors/AdminViewDoctors.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+//view
+import View from "../../view/View";
+
+//components
+import SearchBar from "../../searchBar/SearchBar";
+import CreateDoctorRegisterView from "../DoctorRegisterView/createDoctorRegisterView/CreateDoctorRegisterView";
+import SearchDropDown from "../DoctorRegisterView/registerCard/SearchDropDown/SearchDropDown";
+
+//icons
+import menu_doctor from "../../../assets/menu-doctor.svg";
+import menu_entidad from "../../../assets/menu-entidad.svg";
+import signOut from "../../../assets/SignOut.svg";
+
+//style
+import "./AdminViewDoctors.css";
+
+const AdminViewDoctors = () => {
+    const [search, setSearch] = useState("");
+
+    const menuOptions = [
+        [menu_doctor, "doctores"],
+        [menu_entidad, "entidades"],
+        [signOut, "salir"],
+    ];
+
+    const onSearchChange = (e) => {
+        setSearch(e.target.value);
+        console.log(search);
+    };
+
+    return (
+        <View menuOptions={menuOptions} key={0}>
+            <div className="viewContainer">
+                <div className="searchBarContainer">
+                    <SearchBar />
+                </div>
+                <div className="doctorsContainer">
+                    <CreateDoctorRegisterView search={""}/>
+                </div>
+            </div>
+        </View>
+    );
+};
+
+export default AdminViewDoctors;
