5114083d314472986ca6f1a1726dd922858d9d00
Marco Ramirez <mvrcentes@hotmail.com>
2023-03-31 12:54:04 -0600

    add links

--- consultas-medicas/src/components/view/View.jsx
+++ consultas-medicas/src/components/view/View.jsx
@@ -1,30 +1,29 @@
 //funciones
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 //style
-import './View.css'
+import "./View.css";
 
 //iconos
-import menu_doctor from '../../assets/menu-doctor.svg'
-import menu_entidad from '../../assets/menu-entidad.svg'
-import signOut from '../../assets/SignOut.svg'
+import menu_doctor from "../../assets/menu-doctor.svg";
+import menu_entidad from "../../assets/menu-entidad.svg";
+import signOut from "../../assets/SignOut.svg";
 
-
-import EntityForm from "../AdminView/entityForm/EntityForm"
-import CreateHospitalView from "../EntityView/EntityView/CreateEntityView"
+import EntityForm from "../AdminView/entityForm/EntityForm";
+import CreateHospitalView from "../EntityView/EntityView/CreateEntityView";
 
 const ButtonMenu = ({ icon, text }) => {
     return (
-        <div className="buttonMenu">
+        <button className="buttonMenu">
             <div className="buttonMenuContainer">
                 <img className="icon" src={icon}></img>
                 <div className="text">{text}</div>
             </div>
-        </div>
+        </button>
     );
 };
 
-function View({menuOptions, tipo, children}) {
+function View({ menuOptions, tipo, children }) {
     return (
         <div className="view">
             <div className="menuView">
@@ -32,14 +31,20 @@ function View({menuOptions, tipo, children}) {
                     <Link to="/">
                         <h1>Logo</h1>
                     </Link>
-                    {menuOptions.map((opcion, index) => (<ButtonMenu key={index} icon={opcion[0]} text={opcion[1]} />))}
+                    {menuOptions.map((opcion, index) => (
+                        <Link to={opcion[2]}>
+                            <ButtonMenu
+                                key={index}
+                                icon={opcion[0]}
+                                text={opcion[1]}
+                            />
+                        </Link>
+                    ))}
                 </div>
             </div>
             <div className="rightSide">
                 <div className="fondo">
-                    <div className={`fondoContainer ${tipo}`}>
-                        {children}
-                    </div>
+                    <div className={`fondoContainer ${tipo}`}>{children}</div>
                 </div>
             </div>
         </div>
