1f81b321365a0db8a9ae1e472e7c3d1623c01d42
Marco Ramirez <mvrcentes@hotmail.com>
2023-03-31 12:52:29 -0600

    add menuOptions universal use

---
+++ consultas-medicas/src/components/AdminView/menuOptions.jsx
@@ -0,0 +1,12 @@
+//iconos
+import menu_doctor from "../../assets/menu-doctor.svg";
+import menu_entidad from "../../assets/menu-entidad.svg";
+import signOut from "../../assets/SignOut.svg";
+
+const menuOptions = [
+    [menu_doctor, "doctores", "/adminviewdoctors"],
+    [menu_entidad, "entidades", "/adminviewentities"],
+    [signOut, "salir"],
+];
+
+export default menuOptions
