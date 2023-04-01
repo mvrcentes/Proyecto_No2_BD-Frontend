9e9d3f039c460c4ff55ef4bd5f7fa405f1436d23
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-01 12:57:06 -0600

    add more routes

--- consultas-medicas/src/App.jsx
+++ consultas-medicas/src/App.jsx
@@ -5,36 +5,47 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Menu from './components/menu/Menu'
-import RegisterView from './components/results/registerView/RegisterView'
 import Login from './pages/Login/Login'
 
 // import CreateUser from './components/CreateUser'
 import HospitalsView from './components/EntityView/EntityView/EntitiesView'
 
+//admin views
 import AdminViewEntities from './pages/adminViewEntities/AdminViewEntities'
 import AdminViewDoctors from './pages/adminViewDoctors/AdminViewDoctors'
-import CreateRegisterView from './components/AdminView/DoctorRegisterView/createDoctorRegisterView/CreateDoctorRegisterView'
-import RegisterCard from './components/AdminView/DoctorRegisterView/registerCard/RegisterCard'
 
-import SearchDropDown from './components/AdminView/DoctorRegisterView/registerCard/SearchDropDown/SearchDropDown'
+//employee views
+import EmloyeeView from './components/EmployeeView/EmloyeeView'
+
+//patient view
+import RegisterView from './components/Patient/registerView/RegisterView'
+import ConsultPage from './pages/PatientPages/ConsultPage/ConsultPage'
+import MoreInfo from './pages/PatientPages/MoreInfo/MoreInfo'
+import EntitiesPage from './pages/PatientPages/EntitiesPage/EntitiesPage'
 
 function App() {
     return (
         <div className="App">            
             <Routes>
+                {/* main routes */}
                 <Route path="/" element={<Menu />}></Route>
                 <Route path="/login" element={ <Login /> }></Route>
-                
                 <Route path="/hospitals" element={ <HospitalsView /> }></Route>
                 {/* <Route path="/hospitalForm" element={ <EntityForm /> }></Route> */}
-
                 {/* <Route path="/user" element={ <CreateUser />} ></Route> */}
-
-                <Route path="/results/:id" element={ <RegisterView />} ></Route>
 
                 {/* Routes for Admin */}
                 <Route path="/adminviewentities" element={ <AdminViewEntities />} ></Route>
                 <Route path="/adminviewdoctors" element={ <AdminViewDoctors />} ></Route>
+
+                {/* Routes for employee */}
+                <Route path="/employeeView" element={ <EmloyeeView />} ></Route>
+
+                {/* Routes for patient */}
+                <Route path="/results/:id" element={ <RegisterView />} ></Route>
+                <Route path="/results/:id/consult" element={ <ConsultPage />} ></Route>
+                <Route path="/results/:id/moreinfo" element={ <MoreInfo />} ></Route>
+                <Route path="/results/:id/entities" element={ <EntitiesPage />} ></Route>
             </Routes>
         </div>
     )
