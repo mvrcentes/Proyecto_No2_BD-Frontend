f7cce982e2d15b9ad491d5e4c52b548221bbc060
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-01 13:07:15 -0600

    add RegisterView

---
+++ consultas-medicas/src/components/Patient/registerView/RegisterView.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+//components
+import Navigation from "../../navigation/Navigation";
+import CreateRegisterView from "./CreateRegisterView";
+import SearchBar from "../../searchBar/SearchBar";
+
+//style
+import "./registerView.css";
+
+const registerView = () => {
+    const [search, setSearch] = useState("");
+
+    const onSearchChange = (e) => {
+        setSearch(e.target.value);
+    };
+
+    return (
+        <div className="registerView">
+            <Navigation />
+            <div className="searchBar">
+                <SearchBar key="2" value={search} onChange={onSearchChange}/>
+            </div>
+            <div className="CreateRegisterViewContainer">
+                <CreateRegisterView key="3" search={search}/>
+            </div>
+        </div>
+    );
+};
+
+export default registerView;
