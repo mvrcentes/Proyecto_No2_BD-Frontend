eba55b6291611b0e2162ea5d8c151bb7f445743a
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-01 13:02:36 -0600

    add menuOptions for patient

---
+++ consultas-medicas/src/pages/PatientPages/menuOptions.jsx
@@ -0,0 +1,12 @@
+//icons
+import consulta from "../../assets/consulta.svg";
+import menu_entidad from "../../assets/menu-entidad.svg";
+import SignOut from "../../assets/SignOut.svg";
+
+const menuOptions = [
+    [consulta, "Consultas", "/results/:id/consult"],
+    [menu_entidad, "Entidades", "/results/:id/entities"],
+    [SignOut, "salir"],
+];
+
+export default menuOptions
