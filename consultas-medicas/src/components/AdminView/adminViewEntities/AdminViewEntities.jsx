ab534f9bd704e619881a1cea173fdedf9181b738
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-10 22:16:11 -0600

    Add AdminViewEntities

---
+++ consultas-medicas/src/components/AdminView/adminViewEntities/AdminViewEntities.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+//view
+import View from "../view/View";
+
+//components
+import EntityForm from "./entityForm/EntityForm";
+import CreateHospitalView from "../EntityView/hospitalsView/CreateHospitalView"
+import SearchBar from "../searchBar/SearchBar"
+
+//iconos
+import menu_doctor from "../../assets/menu-doctor.svg"
+import menu_entidad from "../../assets/menu-entidad.svg"
+import signOut from "../../assets/SignOut.svg"
+
+//style
+import "./adminViewEntities.css"
+
+const AdminViewEntities = () => {
+    const [search, setSearch] = useState("")
+    
+    const menuOptions = [
+        [menu_doctor, "doctores"],
+        [menu_entidad, "entidades"],
+        [signOut, "salir"],
+    ]
+
+    const onSearchChange = (e) => {
+        setSearch(e.target.value)
+        console.log(search)
+    }
+
+    return (
+        <View menuOptions={menuOptions} tipo="row" key={0}> 
+            <div className="viewContainer">
+                <div className="entityFormContainer">
+                    <EntityForm />
+                </div>
+                <div className="RightSideComponent">
+                <div className="rightSideContainer">
+                    <div className="searchBarContainer">
+                        <SearchBar value={search} onChange={onSearchChange}/>
+                    </div>
+                    <div className="createHospitalViewContainer">
+                        <CreateHospitalView search={search}/>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </View>
+    )
+}
+
+export default AdminViewEntities
