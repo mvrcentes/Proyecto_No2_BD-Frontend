759de68452f29ccea4ad9aa69a52a525b6552e3b
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-14 23:18:21 -0600

    Add EmployeeViewReports

---
+++ consultas-medicas/src/pages/EmployeeView/EmployeeViewReports/EmployeeViewReports.jsx
@@ -0,0 +1,14 @@
+import View from "../../../components/view/View"
+
+import menuOptions from "../../../components/EmployeeView/Menu"
+
+import "./EmployeeViewReports.css"
+const EmployeeViewReports = () => {
+  return (
+    <View menuOptions={menuOptions}>
+
+    </View>
+  )
+}
+
+export default EmployeeViewReports
