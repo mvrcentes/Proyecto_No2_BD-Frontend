b1abefeda55d86335000a4f7209600e7ccd67927
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-15 13:43:48 -0600

    Add EmployeeViewPatients view

--- consultas-medicas/src/pages/EmployeeView/EmployeeViewPatients/EmployeeViewPatients.jsx
+++ consultas-medicas/src/pages/EmployeeView/EmployeeViewPatients/EmployeeViewPatients.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react"
+
 import View from "../../../components/view/View"
 
 import menuOptions from "../../../components/EmployeeView/Menu"
 
+import PatientRegistersView from "../../../components/EmployeeView/PatientRegistersView/PatientRegistersView"
+import SearchBar from "../../../components/searchBar/SearchBar"
+
 import "./EmployeeViewPatients.css"
 
 const EmployeeViewPatients = () => {
-  return (
-    <View menuOptions={menuOptions}>
-        
-    </View>
-  )
+    const [search, setSearch] = useState("")
+
+    const onSearchChange = (e) => {
+        setSearch(e.target.value)
+     
+    }
+
+    return (
+        <View menuOptions={menuOptions}>
+            <div className="EmployeeViewPatients">
+                <SearchBar value={search} onChange={onSearchChange}/>
+                <PatientRegistersView search={search}/>
+            </div>
+        </View>
+    )
 }
 
 export default EmployeeViewPatients
