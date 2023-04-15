38962c8dcacdaf3190416229b52b6b226b1167a5
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-15 12:55:44 -0600

    Add PatientRegister card

---
+++ consultas-medicas/src/components/EmployeeView/PatientRegistersView/PatientRegisters/PatientRegisters.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+import "./PatientRegisters.css"
+
+const PatientRegisters = ( { data } ) => {
+
+    return (
+        <div className="register">
+            <div className="registerContainer">
+                <div className="dpi">{data.dpi.replace(/(\d{4})(\d{5})(\d{4})/, "$1 $2 $3")}</div>
+                <div className="nombre">{data.nombre}</div>
+                <div className="telefono">{data.telefono.replace(/(\d{4})(\d{4})/, "$1 - $2")}</div>
+                <div className="ver">
+                    <Link to="/results/:id/moreinfo">Visualizar</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PatientRegisters
