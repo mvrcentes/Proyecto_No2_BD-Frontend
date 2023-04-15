0e68691a3c2dd9fc7d30189435fce9243a209b8a
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-14 23:10:48 -0600

    Add menu for EmployeeView

---
+++ consultas-medicas/src/components/EmployeeView/Menu.jsx
@@ -0,0 +1,13 @@
+//iconos
+
+import Consulta from "../../assets/consulta.svg";
+import Reporte from "../../assets/reporte.svg";
+import SignOut from "../../assets/SignOut.svg";
+
+const menuOptions = [
+    [Consulta, "Pacientes", "/adminviewdoctors"],
+    [Reporte, "Reportes", "/adminviewentities"],
+    [SignOut, "Cerrar sesión", "/"],
+]
+
+export default menuOptions
