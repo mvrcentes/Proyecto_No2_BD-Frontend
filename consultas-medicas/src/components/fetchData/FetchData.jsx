0d70c9ae7c52b894d4a53aa14e2864fe009650fd
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-16 14:18:56 -0600

    Add incidencias data

--- consultas-medicas/src/components/fetchData/FetchData.jsx
+++ consultas-medicas/src/components/fetchData/FetchData.jsx
@@ -7,15 +7,41 @@ let FetchData = {}
 const patients = await axios.get(server + "/api/Patients")
 FetchData.getPatients = patients.data
 
+
+export const patientByDPI = async (dpi) => {
+    console.log("PatientBYID", dpi)
+    console.log("PatientBYID", (await axios.get(`${server}/api/Patients/${dpi}`)).data)
+    console.log((await (axios.get(`${server}/api/Patients/${dpi}`))).data)
+    try {
+        return (await axios.get(`${server}/api/Patients/${dpi}`)).data
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 const entities = await axios.get(server + "/api/entities")
 FetchData.getEntities = entities.data
 
-// const treatments = await axios.get(server + "/api/treatment")
-// FetchData.getTreatments = treatments.data
-const incidenceJoined = await axios.get(server + "/api/incidence/joined")
-FetchData.getIncidenceJoined = incidenceJoined.data
+
+
+export const incidences = async (dpi) => {
+    try {
+        return (await axios.get(`${server}/api/incidences/${dpi}`)).data
+    } catch (error) {
+        console.error(error)
+    }
+}
 
 const doctors = await axios.get(server + "/api/usersEmployee")
 FetchData.getDoctors = doctors.data
+
+// Treatments
+const getTreatment = async (id) => {
+    axios.get(
+        server +
+            `/api/employeeViewPatientsemployeeViewPatients/${id}/moreinfo/treatment`
+    )
+}
+FetchData.getTreatment = getTreatment
 
 export default FetchData
