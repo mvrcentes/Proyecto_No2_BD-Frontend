1784f6632449081c05ec2244994ef848c8589281
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-16 16:42:17 -0600

    Add ViewPatientTreatments

---
+++ consultas-medicas/src/pages/EmployeeView/EmployeeViewPatients/ViewPatientTreatments/ViewPatientTreatments.jsx
@@ -0,0 +1,15 @@
+import EmployeeView from "../../../../components/view/EmployeeView"
+
+import CreateInfoCardView from "../../../../components/Patient/createInfoCardView/CreateInfoCardView"
+
+import "./ViewPatientTreatments.css"
+
+const ViewPatientTreatments = () => {
+  return (
+    <EmployeeView>
+        <CreateInfoCardView />
+    </EmployeeView>
+  )
+}
+
+export default ViewPatientTreatments
