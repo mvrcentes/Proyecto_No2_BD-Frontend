882bcb73672f518202e9079d9669da95efb0011e
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-15 18:44:30 -0600

    Add incidenceJoined data

--- consultas-medicas/src/components/fetchData/FetchData.jsx
+++ consultas-medicas/src/components/fetchData/FetchData.jsx
@@ -4,11 +4,16 @@ const server = "http://192.168.1.13:4000"
 
 let FetchData = {}
 
+const patients = await axios.get(server + "/api/Patients")
+FetchData.getPatients = patients.data
+
 const entities = await axios.get(server + "/api/entities")
 FetchData.getEntities = entities.data
 
-const treatments = await axios.get(server + "/api/treatment")
-FetchData.getTreatments = treatments.data
+// const treatments = await axios.get(server + "/api/treatment")
+// FetchData.getTreatments = treatments.data
+const incidenceJoined = await axios.get(server + "/api/incidence/joined")
+FetchData.getIncidenceJoined = incidenceJoined.data
 
 const doctors = await axios.get(server + "/api/usersEmployee")
 FetchData.getDoctors = doctors.data
