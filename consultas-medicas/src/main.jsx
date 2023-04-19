dbf97ceeccc109ff4528727cdd3489742f4db807
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-18 22:33:59 -0600

    Add RolProvider

--- consultas-medicas/src/main.jsx
+++ consultas-medicas/src/main.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./index.css"
 
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom"
+import { RolProvider } from "./contexts/RolProvider"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter> 
-        <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RolProvider>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </RolProvider>
+    </React.StrictMode>
 )
