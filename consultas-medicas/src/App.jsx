e4264c22a2431bf4ac340599100325adfc5a51c5
Marco Ramirez <mvrcentes@hotmail.com>
2023-04-20 20:32:22 -0600

    Remove dead code

--- consultas-medicas/src/App.jsx
+++ consultas-medicas/src/App.jsx
@@ -6,8 +6,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import Menu from './components/menu/Menu'
 import Login from './pages/Login/Login'
-
-// import CreateUser from './components/CreateUser'
 import HospitalsView from './components/EntityView/EntityView/EntitiesView'
 
 //admin views
@@ -20,7 +18,6 @@ import EmployeeViewPatients from './pages/EmployeeView/EmployeeViewPatients/Empl
 import ViewPatientMoreInfo from './pages/EmployeeView/EmployeeViewPatients/ViewPatientMoreInfo/ViewPatientMoreInfo'
 import TraetmentAccordion from './components/EmployeeView/TraetmentAccordion/TraetmentAccordion'
 import ViewPatientTreatments from './pages/EmployeeView/EmployeeViewPatients/ViewPatientTreatments/ViewPatientTreatments'
-
 import EmployeeViewReports from './pages/EmployeeView/EmployeeViewReports/EmployeeViewReports'
 
 //patient view
@@ -29,7 +26,16 @@ import ConsultPage from './pages/PatientPages/ConsultPage/ConsultPage'
 import MoreInfo from './pages/PatientPages/MoreInfo/MoreInfo'
 import EntitiesPage from './pages/PatientPages/EntitiesPage/EntitiesPage'
 
+import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes'
+
+//contexts
+import { useRolContext } from './contexts/RolProvider'
+
+import Modalll from './components/Modal/Modalll'
+
 function App() {
+    const { rol } = useRolContext();
+
     return (
         <div className="App">            
             <Routes>
@@ -37,21 +43,26 @@ function App() {
                 <Route path="/" element={<Menu />}></Route>
                 <Route path="/login" element={ <Login /> }></Route>
                 <Route path="/entities" element={ <HospitalsView /> }></Route>
-                {/* <Route path="/hospitalForm" element={ <EntityForm /> }></Route> */}
-                {/* <Route path="/user" element={ <CreateUser />} ></Route> */}
+                <Route path="/modal" element={ <Modalll /> }></Route>3
+
 
                 {/* Routes for Admin */}
-                <Route path="/adminviewentities" element={ <AdminViewEntities />} />
-                <Route path="/adminviewdoctors" element={ <AdminViewDoctors />} />
-                <Route path="/adminviewreports" element={ <AdminViewReports />} />
+                <Route element={<ProtectedRoutes rol={rol} needs={0} redirectTo='/login'/>} >
+                    <Route path="/adminviewentities" element={ <AdminViewEntities />} />
+                    <Route path="/adminviewdoctors" element={ <AdminViewDoctors />} />
+                    <Route path="/adminviewreports" element={ <AdminViewReports />} />
+                </Route>
 
                 {/* Routes for employee */}
-                <Route path="/employeeViewPatients" element={ <EmployeeViewPatients />} />
-                <Route path="/employeeViewPatients/:dpi" element={ <ViewPatientMoreInfo />} />
-                <Route path="/employeeViewPatients/:dpi/incidence/:id" element={ <ViewPatientTreatments />} />
-                <Route path="/employeeViewReport" element={ <EmployeeViewReports />} />
-                
+                <Route element={<ProtectedRoutes rol={rol} needs={1} redirectTo='/login'/>} >
+                    <Route path="/employeeViewPatients" element={ <EmployeeViewPatients />} />
+                    <Route path="/employeeViewPatients" element={ <EmployeeViewPatients />} />
+                    <Route path="/employeeViewPatients/:dpi" element={ <ViewPatientMoreInfo />} />
+                    <Route path="/employeeViewPatients/:dpi/incidence/:id" element={ <ViewPatientTreatments />} />
+                    <Route path="/employeeViewReport" element={ <EmployeeViewReports />} />
+                </Route>
 
+                
                 {/* Routes for patient */}
                 <Route path="/results/:id" element={ <RegisterView />} />
                 <Route path="/results/:id/consult" element={ <ConsultPage />} />
